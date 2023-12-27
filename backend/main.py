3940fe32533920ad41b1bb6860a207b291ecea53
Salmaan Saeed <salsaeed999@gmail.com>
2023-12-27 00:32:52 -0500

    created the book search by subject api

--- backend/main.py
+++ backend/main.py
@@ -20,6 +20,7 @@ def convert_array_to_string(list_of_items):
     string_list = ",".join(str(x) for x in list_of_items)
     return string_list
 
+
 @app.route("/search")
 def book_search():
     # to get the value of query (i.e. ?q=some-value)
@@ -62,6 +63,36 @@ def book_search():
 
         if 'id_google' in item:
             book['id_google'] = convert_array_to_string(item['id_google'])
+
+        books.append(book)
+
+    return {"books": books}
+
+
+@app.route("/search_by_subject")
+def book_search_by_subject():
+    # to get the value of query (i.e. ?q=some-value)
+    # https://openlibrary.org/search.json?q=flyte
+    query = request.args.get("q")
+    response = requests.get("https://openlibrary.org/subjects/" + query + ".json")
+    data = response.json()
+
+    books = []
+
+    for item in data["works"]:
+        book = {}
+
+        if 'title' in item:
+            book['title'] = item['title']
+
+        if 'authors' in item:
+            book['author_name'] = item['authors'][0]['name']
+
+        if 'number_of_pages_median' in item:
+            book['number_of_pages_median'] = item['number_of_pages_median']
+
+        if 'first_publish_year' in item:
+            book['first_publish_year'] = item['first_publish_year']
 
         books.append(book)
 
