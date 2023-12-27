7a5990f3ecc2c6efef8054a953a77f824f716721
Salmaan Saeed <salsaeed999@gmail.com>
2023-12-26 19:48:59 -0500

    removed useEffect import

--- mobile/components/pages/ViewBooks.js
+++ mobile/components/pages/ViewBooks.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import axios from 'axios';
 import { View, FlatList, Text, TextInput, Button, ActivityIndicator} from 'react-native';
 
