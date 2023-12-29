cf9afed67bbcd07a9ad7776d5c51662d416f2769
Salmaan Saeed <salsaeed999@gmail.com>
2023-12-28 20:58:56 -0500

    added tab navigation to the app

--- mobile/App.js
+++ mobile/App.js
@@ -1,19 +1,21 @@
 import ViewBooks from './components/pages/ViewBooks';
 import ViewBooksBySubject from './components/pages/ViewBooksBySubject';
 import ViewBooksByAuthor from './components/pages/ViewBooksByAuthor';
-import {NavigationContainer} from '@react-navigation/native';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
-const Stack = createNativeStackNavigator();
+// const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
 
 export default function App() {
     return (
         <NavigationContainer>
-            <Stack.Navigator initialRouteName="Books">
-                <Stack.Screen name="Books" component={ViewBooks} />
-                <Stack.Screen name="Subjects" component={ViewBooksBySubject} />
-                <Stack.Screen name="Author" component={ViewBooksByAuthor} />
-            </Stack.Navigator>
+            <Tab.Navigator initialRouteName="Books">
+                <Tab.Screen name="Books" component={ViewBooks} />
+                <Tab.Screen name="Subjects" component={ViewBooksBySubject} />
+                <Tab.Screen name="Author" component={ViewBooksByAuthor} />
+            </Tab.Navigator>
         </NavigationContainer>
   );
 }
