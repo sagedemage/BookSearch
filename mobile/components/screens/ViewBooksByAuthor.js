ad9f5819f2073f960fb03dfe2d7a8d7f62d398bb
Salmaan Saeed <salsaeed999@gmail.com>
2023-12-29 20:42:32 -0500

    adjusted the look of the tables

--- mobile/components/screens/ViewBooksByAuthor.js
+++ mobile/components/screens/ViewBooksByAuthor.js
@@ -26,13 +26,13 @@ export default function ViewBooksByAuthor({navigation}) {
     const _renderItem = ({ item }) => (
         <View style={{ flexDirection: 'row', borderBottomWidth: 2, borderLeftWidth: 2, borderRightWidth: 2}}>
             {/* Only 3 or 4 items a time */}
-            <View style={{width: 100, borderRightWidth: 2}}>
+            <View style={{width: 120, padding: 5, borderRightWidth: 2}}>
                 <Text style={{textAlign: 'center'}}>{item.title}</Text>
             </View>
-            <View style={{width: 100, borderRightWidth: 2}}>
+            <View style={{width: 120, padding: 5,  borderRightWidth: 2}}>
                 <Text style={{textAlign: 'center'}}>{item.first_publish_year}</Text>
             </View>
-            <View style={{width: 100, borderRightWidth: 2}}>
+            <View style={{width: 120, padding: 5, borderRightWidth: 2}}>
                 <Text style={{textAlign: 'center'}}>{item.number_of_pages_median}</Text>
             </View>
         </View>
@@ -54,13 +54,13 @@ export default function ViewBooksByAuthor({navigation}) {
             </View>
             <View style={{flex: 1, justifyContent: 'center', alignItems: 'center', marginTop: '5%'}}>
                 <View style={{ flexDirection: 'row', borderBottomWidth: 2, borderLeftWidth: 2, borderRightWidth: 2}}>
-                    <View style={{width: 100, backgroundColor: 'black'}}>
+                    <View style={{width: 120, padding: 5, backgroundColor: 'black'}}>
                         <Text style={{textAlign: 'center', color: 'white'}}>Title</Text>
                     </View>
-                    <View style={{width: 100, backgroundColor: 'black'}}>
+                    <View style={{width: 120, padding: 5, backgroundColor: 'black'}}>
                         <Text style={{textAlign: 'center', color: 'white'}}>First Publish Year</Text>
                     </View>
-                    <View style={{width: 100, backgroundColor: 'black'}}>
+                    <View style={{width: 120, padding: 5, backgroundColor: 'black'}}>
                         <Text style={{textAlign: 'center', color: 'white'}}>Number of Pages Median</Text>
                     </View>
                 </View>
--- mobile/components/screens/ViewBooksBySubject.js
+++ mobile/components/screens/ViewBooksBySubject.js
@@ -26,13 +26,13 @@ export default function ViewBooksBySubject({navigation}) {
     const _renderItem = ({ item }) => (
         <View style={{ flexDirection: 'row', borderBottomWidth: 2, borderLeftWidth: 2, borderRightWidth: 2}}>
             {/* Only 3 or 4 items a time */}
-            <View style={{width: 100, borderRightWidth: 2}}>
+            <View style={{width: 120, padding: 5, borderRightWidth: 2}}>
                 <Text style={{textAlign: 'center'}}>{item.title}</Text>
             </View>
-            <View style={{width: 100, borderRightWidth: 2}}>
+            <View style={{width: 120, padding: 5, borderRightWidth: 2}}>
                 <Text style={{textAlign: 'center'}}>{item.author_name}</Text>
             </View>
-            <View style={{width: 100, borderRightWidth: 2}}>
+            <View style={{width: 120, padding: 5, borderRightWidth: 2}}>
                 <Text style={{textAlign: 'center'}}>{item.first_publish_year}</Text>
             </View>
         </View>
@@ -54,13 +54,13 @@ export default function ViewBooksBySubject({navigation}) {
             </View>
             <View style={{flex: 1, justifyContent: 'center', alignItems: 'center', marginTop: '5%'}}>
                 <View style={{ flexDirection: 'row', borderBottomWidth: 2, borderLeftWidth: 2, borderRightWidth: 2}}>
-                    <View style={{width: 100, backgroundColor: 'black'}}>
+                    <View style={{width: 120, padding: 5, backgroundColor: 'black'}}>
                         <Text style={{textAlign: 'center', color: 'white'}}>Title</Text>
                     </View>
-                    <View style={{width: 100, backgroundColor: 'black'}}>
+                    <View style={{width: 120, padding: 5, backgroundColor: 'black'}}>
                         <Text style={{textAlign: 'center', color: 'white'}}>Author Name</Text>
                     </View>
-                    <View style={{width: 100, backgroundColor: 'black'}}>
+                    <View style={{width: 120, padding: 5, backgroundColor: 'black'}}>
                         <Text style={{textAlign: 'center', color: 'white'}}>First Publish Year</Text>
                     </View>
                 </View>
