7f9e15b4e4e94e7eac8f0242ea9c907423fabf88
Salmaan Saeed <salsaeed999@gmail.com>
2024-01-01 01:42:48 -0500

    added ui tests for the app and view books by subject components

---
+++ mobile/__tests__/app_ui.test.js
@@ -0,0 +1,33 @@
+import * as React from 'react';
+import { screen, render, fireEvent, cleanup } from '@testing-library/react-native';
+import App from '../App';
+
+test('subject tab navigation', () => {
+    render(
+        <App />
+    );
+
+    /* Subject Navigation Tab */
+    let element = screen.getByLabelText("Subject, tab, 2 of 3")
+
+    expect(element.props.accessibilityLabel).toContain('Subject, tab, 2 of 3');
+    expect(element.props.accessibilityRole).toContain('button');
+    expect(element.props.style[1].backgroundColor).toContain('#34455D');
+    cleanup();
+})
+
+test('subject header', () => {
+    render(
+        <App />
+    );
+
+    fireEvent.press(screen.getByLabelText('Subject, tab, 2 of 3'));
+
+    /* Subject Header */
+    let element = screen.getByRole('header', {name: 'Subject'})
+
+    expect(element.props.children).toContain('Subject');
+    expect(element.props.accessibilityRole).toContain('header');
+    expect(element.props.style.color).toContain('white');
+    cleanup();
+})
---
+++ mobile/__tests__/view_books_by_subject_ui.test.js
@@ -0,0 +1,20 @@
+import * as React from 'react';
+import { screen, render, cleanup } from '@testing-library/react-native';
+import ViewBooksBySubject from '../components/screens/ViewBooksBySubject';
+
+test('text input', () => {
+    render(
+        <ViewBooksBySubject />
+    );
+
+    /* Text Input */
+    element = screen.getByPlaceholderText('Search Books by Subject');
+
+    expect(element.props.placeholder).toContain("Search Books by Subject");
+    expect(element.props.defaultValue).toContain("");
+    expect(element.props.style.borderWidth).toBe(2);
+    expect(element.props.style.height).toBe(35);
+    expect(element.props.style.paddingHorizontal).toBe(10);
+    expect(element.props.style.width).toBe(200);
+    cleanup();
+})
