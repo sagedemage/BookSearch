c249433ae2bbe6fa7e7e6f84689f1ace5aaddaea
Salmaan Saeed <salsaeed999@gmail.com>
2024-01-01 22:48:31 -0500

    add a comment for each test api

--- backend/routes/test_page.py
+++ backend/routes/test_page.py
@@ -1,16 +1,17 @@
-from flask import Blueprint, request
-import requests
+from flask import Blueprint
 
 test_page = Blueprint('test_page', __name__)
 
 
 @test_page.route("/")
 def home():
+    """Test rendering the HTML content for the index route"""
     html = "<h1>Home Page</h1>\n <p>This is the home page</p>"
     return html
 
 
 @test_page.route("/hello")
 def hello_world():
+    """Test fetching the json data"""
     data = {"msg": "Request received", "type": "success"}
     return data
