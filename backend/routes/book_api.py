34c6636005159a30a721ba94cb29f0b0854e37b0
Salmaan Saeed <salsaeed999@gmail.com>
2024-01-01 22:47:47 -0500

    add a comment for the function to convert an array to a string

--- backend/routes/book_api.py
+++ backend/routes/book_api.py
@@ -5,6 +5,7 @@ book_api = Blueprint('book_api', __name__)
 
 
 def convert_array_to_string(list_of_items):
+    """Convert an array to a string"""
     string_list = ",".join(str(x) for x in list_of_items)
     return string_list
 
